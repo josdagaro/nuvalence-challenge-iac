9d948b0b4143a96e932d0086d08798f0575d990a
José David García Rodríguez <josdagaro@gmail.com>
2022-03-16 00:36:08 -0500

    Add database

--- rds_postgresql.tf
+++ rds_postgresql.tf
@@ -1,4 +1,4 @@
-module "security_group" {
+module "security_group_db" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
@@ -24,5 +24,67 @@ module "security_group" {
 
   tags = {
     Env = var.env
+  }
+}
+
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  providers = {
+    aws = aws.ohio
+  }
+
+  create_db_instance                    = var.create
+  create_db_option_group                = var.create
+  create_db_parameter_group             = var.create
+  create_db_subnet_group                = var.create
+  identifier                            = "${var.env}-nuva-db"
+  engine                                = "postgres"
+  engine_version                        = "10.20"
+  family                                = "postgres10"
+  major_engine_version                  = "10"
+  instance_class                        = "db.t4g.large"
+  allocated_storage                     = 20
+  max_allocated_storage                 = 100
+  db_name                               = "nuva"
+  username                              = var.db_secret_user
+  port                                  = 5432
+  multi_az                              = true
+  db_subnet_group_name                  = module.vpc.database_subnet_group
+  vpc_security_group_ids                = [module.security_group_db.security_group_id]
+  maintenance_window                    = "Mon:00:00-Mon:03:00"
+  backup_window                         = "03:00-06:00"
+  enabled_cloudwatch_logs_exports       = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group           = var.create
+  backup_retention_period               = 0 # In days
+  skip_final_snapshot                   = true
+  deletion_protection                   = false
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7 # In days
+  create_monitoring_role                = var.create
+  monitoring_interval                   = 60 # In seconds
+  monitoring_role_name                  = "nuva-db-monitoring"
+  monitoring_role_description           = "Description for monitoring role"
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
+    }
+  ]
+
+  tags = {
+    Env = var.env
+  }
+
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
   }
 }
