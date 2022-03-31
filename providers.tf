d304c7ff4af61a3812ac5051f8cd271bea6b1336
José David García Rodríguez <josdagaro@gmail.com>
2022-03-31 11:54:00 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,6 +7,6 @@ terraform {
   required_version = ">= 1.1.5, <= 1.1.7"
 
   required_providers {
-    aws = "4.3.0" # Exact version to make sure we don't get undesired updates
+    aws = "4.8.0" # Exact version to make sure we don't get undesired updates
   }
 }
